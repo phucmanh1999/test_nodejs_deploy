8be8d45f9f4b7991c468a0d8e2f33d616624d410
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2020-09-30 22:49:24 +0700

    change content

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Welcome to my web xD');
+    res.send('phuc bu dit');
 })
 app.listen(port,() => {
     console.log('Hi Manh, server is running ...');
