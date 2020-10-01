bb8660bb05b84008daa5a4461c2bb9c8d83767e3
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2020-10-01 09:17:18 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Welcome to my web xD');
-})
-app.listen(port,() => {
-    console.log('Hi Manh, server is running ...');
-});
+    readFile('./home.html','utf8',(err, html)=>{
+        if (err) {
+            response.status(500).send("Sorry, out of order.")
+        }
 
+        response.send(html)
+    })
+});
+app.listen(port,() => {
+    console.log('Hi Manh, server is running...');
+});
